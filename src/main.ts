cd3e7122f33090977a8158ebd3d8e4a912d48390
ashtonjgiles <ashtonjgiles@gmail.com>
2026-04-20 20:42:51 -0600

    feat: move css to correct file

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import router from "./router/router";
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/reset.css";
-import "./assets/main.css";
+import "./style.css";
 import App from "./App.vue";
 
 createApp(App).use(router).use(Antd).mount("#app");
